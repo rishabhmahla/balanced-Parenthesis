e9c79a41a574ef6c75b55b6f383bf69afceceaf6
Rishabh Mahla <rishabh.mahla@gmail.com>
2024-02-20 21:32:10 +0530

    Create balancedCode.cpp

---
+++ balancedCode.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool parenValid(string SomeString){
+    stack<char> ParStack;
+    for (int i=0; i<SomeString.size();i++){
+        if (SomeString[i]=='{' || SomeString[i]=='[' || SomeString[i]=='('){
+            ParStack.push(SomeString[i]);
+            
+        }else {
+            if(SomeString[i]=='}' && ParStack.top()=='{'){
+               ParStack.pop(); 
+            }
+            else if(SomeString[i]==')' && ParStack.top()=='('){
+               ParStack.pop(); 
+            }
+            else if(SomeString[i]==']' && ParStack.top()=='['){
+                ParStack.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        
+    }
+    return true;
+
+}
+
+int main(){
+    string Mystring = "{()()[]}";
+    bool isValid = parenValid(Mystring);
+    if(isValid) {
+        cout<<"Valid"<<endl;
+    }else{
+        cout<<"Invalid"<<endl;
+    }
+
+}
